462a70360495dc129f81969c853e8c3e03240608
Vsevolod-Bro <brvv.job@gmail.com>
2021-04-10 09:20:59 +0700

    add tested app on flask; edit gitignore

--- Exam/app1flask/application.py
+++ Exam/app1flask/application.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 
 @application.route("/")
 def index():
-    return "<h1>Helloo from FLASK</h1>"
+    return "<h1>Hello World 1 (FLASK)</h1>"
+
+@application.route("/an")
+def an():
+    return render_template("anders.html")
+
+@application.route("/user/<string:name>")
+def user(name):
+    return "<h1>User page for User: " + name + "</h1>"
 
 if __name__  == "__main__":
-    application.run()
+    #application.debug = True
+    application.run(host="0.0.0.0")
 
--- Exam/app1flask/wsgi.py
+++
@@ -1,5 +0,0 @@
-from app1flask import web_main
-
-if __name__ == "__main__":
-   web_main.run()
-
